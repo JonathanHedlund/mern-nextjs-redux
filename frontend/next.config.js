aa32050d3d84753bddab665690e000a54d735ac3
JonathanHedlund <jonathan.hedlund.371@gmail.com>
2022-02-18 12:51:08 +0100

    removed bug

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  async rewrites() {
-    console.log("Rewrite")
-    return [
-      {
-        source: '/api',
-        destination: 'http://localhost:5000/api',
-      },
-    ]
-  }
 }
