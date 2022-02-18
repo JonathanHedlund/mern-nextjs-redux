9220d2775a041eae4bbad24b9c752e5134c83627
JonathanHedlund <jonathan.hedlund.371@gmail.com>
2022-02-18 12:44:21 +0100

    Heroku setup

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    console.log("Rewrite")
+    return [
+      {
+        source: '/api',
+        destination: 'http://localhost:5000/api',
+      },
+    ]
+  }
 }
